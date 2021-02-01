16e7b432dac3d98f879349bdb67294ad8955bb5e
Arinonia <arinonia.yt@gmail.com>
2021-02-01 10:50:31 +0100

    start project

---
+++ app/src/main/java/fr/arinonia/app/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.arinonia.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+    }
+}
